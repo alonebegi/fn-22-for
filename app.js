6487c96c6b1ce00a9aaf18ae8d158702f1d7241c
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-31 11:51:41 +0300

    fn 22 for

---
+++ app.js
@@ -0,0 +1,45 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i+=1);
+// }
+// sum = 0;
+// g = 100;
+// for(let i = 1; i <= g; i++){
+//     sum += i
+// }
+// console.log("sum", sum);
+
+
+
+// 1masala
+// let a = 100;
+// for(i = 1; i <= a; i++){
+//     if(i % 2 == 0){
+//         console.log(i, "bu son juft son");
+//     }else{
+//         console.log("bu son toq son");
+//     }
+// }
+
+
+
+// 2masala
+// let gmail = "asadbek21@gmail.com";
+
+// let qoshmchaGmail = prompt("gmailni kriting: ");
+// toString(qoshmchaGmail)
+// if(gmail === qoshmchaGmail){
+//     console.log(siz saytga ottingiz);
+// }else{
+//     console.log(siz saytga otomadingiz boshqattan urunb korn);
+// }
+
+// 4masala
+// let user = +prompt("1 chi sonni kriting : ");
+// let user2 = +prompt("2 chi sonni kriting: ");
+// for(let i = user; i <= user2; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
